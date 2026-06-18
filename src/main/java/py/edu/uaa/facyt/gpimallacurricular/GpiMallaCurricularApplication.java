e577bd2587b9e13a4d028ddd3a62a9e5a4a452ab
martinovelar <martinovelarseg@gmail.com>
2026-06-17 23:43:05 -0300

    chore: initial spring boot setup

---
+++ src/main/java/py/edu/uaa/facyt/gpimallacurricular/GpiMallaCurricularApplication.java
@@ -0,0 +1,13 @@
+package py.edu.uaa.facyt.gpimallacurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpiMallaCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpiMallaCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uaa/facyt/gpimallacurricular/GpiMallaCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uaa.facyt.gpimallacurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpiMallaCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
