47ac963279c4150563ce101e488c4c25630d6b46
martinovelar <martinovelarseg@gmail.com>
2026-06-20 14:59:35 -0300

    feat: add facultad model

---
+++ src/main/java/py/edu/uaa/facyt/gpimallacurricular/model/Facultad.java
@@ -0,0 +1,26 @@
+package py.edu.uaa.facyt.gpimallacurricular.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "facultad")
+public class Facultad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String dean;
+    @Builder.Default
+    private Boolean active = true;
+
+}
