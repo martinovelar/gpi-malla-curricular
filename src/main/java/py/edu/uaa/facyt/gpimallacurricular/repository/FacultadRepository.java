103179d85a50b4b37744b2009c6144743b39091f
martinovelar <martinovelarseg@gmail.com>
2026-06-20 15:05:12 -0300

    feat: add facultad repository

---
+++ src/main/java/py/edu/uaa/facyt/gpimallacurricular/repository/FacultadRepository.java
@@ -0,0 +1,9 @@
+package py.edu.uaa.facyt.gpimallacurricular.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import py.edu.uaa.facyt.gpimallacurricular.model.Facultad;
+
+
+public interface FacultadRepository extends JpaRepository<Facultad,Long> {
+
+}
