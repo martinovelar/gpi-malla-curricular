57ec4229d4e61f89cb60954dd1001c720f254289
martinovelar <martinovelarseg@gmail.com>
2026-06-20 15:11:09 -0300

    feat: add global exception handler

---
+++ src/main/java/py/edu/uaa/facyt/gpimallacurricular/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package py.edu.uaa.facyt.gpimallacurricular.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
